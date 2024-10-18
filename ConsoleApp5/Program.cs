988440a806060f8300d59f925957d91118db71ba
ilaharustamli <ilahasr-ap118@code.edu.az>
2024-10-18 13:25:36 +0400

    Created

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            bool f = true;
+
+            Console.WriteLine("silah yarat");
+            Console.WriteLine("tutumu daxil et:");
+            int tutum = int.Parse(Console.ReadLine());
+            Console.WriteLine("ilk mermini daxil et:");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("atis modunu daxil et:");
+            string atismodu = Console.ReadLine();
+            Weapon weapon = new Weapon(tutum, sayi, atismodu);
+
+            do
+            {
+               
+                int command = int.Parse(Console.ReadLine());
+                Console.WriteLine("0. İnformasiya almaq üçün, 1.Shoot metodu üçün, 2.GetRemainBulletCount metodu üçün, 3. Reload metodu üçün, 4. ChangeFireMode metodu üçün, 5. Proqramdan dayandırmaq üçün qısayoldur.");
+                
+                switch (command)
+                {
+                    case 0:
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        break;
+
+                        case 1:
+                        weapon.Shoot();
+                        break;
+                        case 2:
+                        weapon.Reload();
+                        break;
+                        case 3:
+                        weapon.Shoot();
+                        break;
+                        case 4:
+                        weapon.ChangeFireMode();
+                        break;
+                        case 5:
+                        f = false;  
+                        Console.WriteLine("program bitti");
+                        break;
+                       default:
+                        Console.WriteLine("yeniden yoxla");
+                        break;
+                        
+                }
+
+            }
+            while (!f);
+        }
+    }
+}
+        
+
+        
+           
---
+++ ConsoleApp5/Weapon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Weapon
+    {
+        private static int _tutum;
+        private int sayi;
+        private bool atismodu;
+        private int tutum;
+        private string atismodu1;
+
+        public Weapon(int tutum, int sayi, bool atismodu)
+        {
+            if (tutum <= 0 || sayi < 0 || sayi > tutum)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            _tutum = tutum;
+            this.sayi = sayi;
+            this.atismodu = atismodu;
+        }
+
+        public Weapon(int tutum, int sayi, string atismodu1)
+        {
+            this.tutum = tutum;
+            this.sayi = sayi;
+            this.atismodu1 = atismodu1;
+        }
+
+        public void Shoot()
+        {
+            if (atismodu)
+            {
+                if (_tutum > sayi)
+                {
+                    sayi--;
+                }
+                else
+                {
+                    Console.WriteLine("daraq bosdur ");
+                }
+            }
+            else {
+
+                Console.WriteLine("atismodu baqlidir");
+
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return tutum - sayi; 
+            
+        }
+
+        public void Reload()
+        {
+            sayi = _tutum;
+        }
+
+        public void ChangeFireMode()
+        {
+            if (atismodu)
+            {
+                Console.WriteLine("atismodu false");
+                atismodu = false;
+            }
+            else {
+                Console.WriteLine("atismodu true");
+                atismodu = true;
+            }
+
+
+        }
+    } }
